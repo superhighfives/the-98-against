f74573a485a921e422f6778d12e342ace406b19c
Charlie Gleason <hi@superhighfives.com>
2012-09-23 03:46:30 +1000

    Changed db name

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ end
 
 configure :production do
   # Database connection
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/the98against')
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
